302869f37e71cb94c328be3e67fe1c94b3702dbe
Mithun <mithunadhikari40@gmail.com>
2022-06-04 22:25:49 +0545

    Just playing around

--- main.py
+++ main.py
@@ -1,20 +1,76 @@
-from utils.console import print_markdown
 import time
+from pathlib import Path
+
+import pyttsx3
+from mutagen.mp3 import MP3
+
 from reddit.askreddit import get_askreddit_threads
+from utils.console import print_markdown, print_step
 from video_creation.background import download_background, chop_background_video
-from video_creation.voices import save_text_to_mp3
-from video_creation.screenshot_downloader import download_screenshots_of_reddit_posts
 from video_creation.final_video import make_final_video
+from video_creation.screenshot_downloader import download_screenshots_of_reddit_posts
+from video_creation.voices import save_text_to_mp3
 
 print_markdown(
-    "### Thanks for using this tool! 😊 [Feel free to contribute to this project on GitHub!](https://lewismenelaws.com). If you have any questions, feel free to reach out to me on Twitter or submit a GitHub issue."
+    "### Thanks for using this tool! 😊 [Feel free to contribute to this project on GitHub!]("
+    "https://lewismenelaws.com). If you have any questions, feel free to reach out to me on "
+    "Twitter or submit a GitHub issue. "
 )
 
+
+def test_voice():
+    print_step("Saving Text to MP3 files 🎶")
+    music_len = 0
+
+    # Create a folder for the mp3 files.
+    Path("assets/mp3").mkdir(parents=True, exist_ok=True)
+
+    # tts = gTTS(text=reddit_obj["thread_title"], lang="en", slow=False, tld="com")
+    # tts.save(f"assets/mp3/title.mp3")
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id)
+    engine.setProperty('volume', 1.0)
+    engine.setProperty('rate', 125)
+    file_path = f"assets/mp3/title.wav"
+    engine.save_to_file("Why are men always paid more than woman even if the job is very much a "
+                        "woman specific, like a nurse", file_path)
+
+    engine.runAndWait()
+    engine.stop()
+    time.sleep(1)
+
+    import soundfile as sf
+    f = sf.SoundFile(file_path)
+    print('samples = {}'.format(f.frames))
+    print('sample rate = {}'.format(f.samplerate))
+    print('seconds = {}'.format(f.frames / f.samplerate))
+
+    file_exists = Path(file_path).exists()
+    is_file = Path(file_path).is_file()
+
+    # music_len += MP3(file_path).info.length
+    print(f"The length of the music file is {music_len}")
+
+
+# def test_method():
+#     import pyttsx3
+#     engine = pyttsx3.init()
+#     voices = engine.getProperty('voices')
+#     for voice in voices:
+#         engine.setProperty('voice', voice.id)
+#         engine.save_to_file('Hello World' , 'test.mp3')
+#
+#         print(voice.id)
+#         engine.say('The quick brown fox jumped over the lazy dog.')
+#     engine.runAndWait()
+
+
 time.sleep(3)
-
-
+# test_method()
+# test_voice()
+#
 reddit_object = get_askreddit_threads()
-
 length, number_of_comments = save_text_to_mp3(reddit_object)
 download_screenshots_of_reddit_posts(reddit_object, number_of_comments)
 download_background()
--- video_creation/voices.py
+++ video_creation/voices.py
@@ -1,8 +1,21 @@
-from gtts import gTTS
+import time
 from pathlib import Path
-from mutagen.mp3 import MP3
-from utils.console import print_step, print_substep
+
+import pyttsx3
+import soundfile as sf
 from rich.progress import track
+
+from utils.console import print_step, print_substep
+
+
+def _init_voice():
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id)
+    engine.setProperty('volume', 1.0)
+    engine.setProperty('rate', 200)
+    engine.runAndWait()
+    return engine
 
 
 def save_text_to_mp3(reddit_obj):
@@ -11,24 +24,51 @@ def save_text_to_mp3(reddit_obj):
     Args:
         reddit_obj : The reddit object you received from the reddit API in the askreddit.py file.
     """
-    print_step("Saving Text to MP3 files 🎶")
-    length = 0
 
-    # Create a folder for the mp3 files.
-    Path("assets/mp3").mkdir(parents=True, exist_ok=True)
+    try:
+        print_step("Saving Text to MP3 files 🎶")
+        length = 0
 
-    tts = gTTS(text=reddit_obj["thread_title"], lang="en", slow=False, tld="co.uk")
-    tts.save(f"assets/mp3/title.mp3")
-    length += MP3(f"assets/mp3/title.mp3").info.length
+        # Create a folder for the mp3 files.
+        Path("assets/mp3").mkdir(parents=True, exist_ok=True)
 
-    for idx, comment in track(enumerate(reddit_obj["comments"]), "Saving..."):
-        # ! Stop creating mp3 files if the length is greater than 50 seconds. This can be longer, but this is just a good starting point
-        if length > 50:
-            break
-        tts = gTTS(text=comment["comment_body"], lang="en")
-        tts.save(f"assets/mp3/{idx}.mp3")
-        length += MP3(f"assets/mp3/{idx}.mp3").info.length
+        # tts = gTTS(text=reddit_obj["thread_title"], lang="en", slow=False, tld="com")
+        # tts.save(f"assets/mp3/title.mp3")
+        file_path = f"assets/mp3/title.wav"
 
-    print_substep("Saved Text to MP3 files Successfully.", style="bold green")
-    # ! Return the index so we know how many screenshots of comments we need to make.
-    return length, idx
+        engine = _init_voice()
+        engine.save_to_file(reddit_obj["thread_title"], file_path)
+        engine.runAndWait()
+        f = sf.SoundFile(file_path)
+        len_in_sec = f.frames / f.samplerate
+        print('seconds = {}'.format(f.frames / f.samplerate))
+
+        # length += MP3(file_path).info.length
+        length += len_in_sec
+
+        for idx, comment in track(enumerate(reddit_obj["comments"]), "Saving..."):
+            # ! Stop creating mp3 files if the length is greater than 50 seconds. This can be longer,
+            # but this is just a good starting point
+            if length > 50:
+                break
+            # tts = gTTS(text=comment["comment_body"], lang="en", slow=False, tld="com")
+            # tts.save(f"assets/mp3/{idx}.mp3")
+            subfile_path = f"assets/mp3/{idx}.wav"
+            print(f"The comment body is {comment['comment_body']}")
+
+            engine.save_to_file(comment["comment_body"], subfile_path)
+            engine.runAndWait()
+
+            f = sf.SoundFile(subfile_path)
+            len_in_sec = f.frames / f.samplerate
+            print(f"The file length is {len_in_sec}")
+            # length += MP3(f"assets/mp3/{idx}.mp3").info.length
+            length += len_in_sec
+
+        print_substep("Saved Text to MP3 files Successfully.", style="bold green")
+        # ! Return the index so we know how many screenshots of comments we need to make.
+        engine.stop()
+        return length, idx
+    except Exception as e:
+        print(f"The raised error {e}")
+        return None, None
